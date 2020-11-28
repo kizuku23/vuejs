a7eaa3358effeb7ca5deb530745fe9c67f039f88
vagrant <km.dwc23@gmail.com>
2020-11-28 05:37:35 +0000

    [Finish] 3-42

--- todo_app/js/main.js
+++ todo_app/js/main.js
@@ -1,3 +1,18 @@
 var app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    newItem: '',
+    todos: []
+  },
+  methods: {
+    addItem: function(event) {
+      // alert()
+      var todo = {
+        item: this.newItem
+      }
+
+      this.todos.push(todo)
+      this.newItem = ''
+    }
+  }
 })
