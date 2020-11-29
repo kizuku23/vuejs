99217a2a58f16bbfe7741fdb2ba9fb20a8c5050c
vagrant <km.dwc23@gmail.com>
2020-11-29 06:41:01 +0000

    [Finish] 5-72

--- sample/js/main.js
+++ sample/js/main.js
@@ -1,8 +1,15 @@
+Vue.filter('numberFormat', function(value) {
+  return value.toLocaleString()
+})
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!',
-    number: 100,
-    ok: true
-  }
+    price: 2980000000
+  },
+  // filters: {
+  //   numberFormat: function(value) {
+  //     return value.toLocaleString()
+  //   }
+  // }
 })
