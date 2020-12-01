d417b4450f98319612259d5a1a37da5a4f2ed7d7
vagrant <km.dwc23@gmail.com>
2020-12-01 04:49:22 +0000

    [Finish] 9-102

--- sample/js/main.js
+++ sample/js/main.js
@@ -1,22 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-    colors: [
-      { name: 'Red' },
-      { name: 'Green' },
-      { name: 'Blue' }
-    ]
-  },
-  watch: {
-    colors: {
-      handler: function(newValue, oldValue) {
-        console.log('Update!')
-        console.log('new: %s, oldValue: %s', 
-          JSON.stringify(newValue, null, '\t'),
-          JSON.stringify(oldValue, null, '\t'))
-      },
-      deep: true,
-      immediate: false
-    }
+    isLarge: true,
+    hasError: true
   }
 })
