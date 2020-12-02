4c046f4bfa99ada39039076c5c86933277f4d17d
vagrant <km.dwc23@gmail.com>
2020-12-02 08:04:46 +0000

    [Finish] 11-118

--- sample/js/main.js
+++ sample/js/main.js
@@ -1,15 +1,12 @@
 var app = new Vue({
   el: '#app',
   data: {
-    counter: 0
+    message: ''
   },
   methods: {
-    clickHandler: function(event) {
-      this.counter++
-      console.log(event)
-      console.log(event.target.tagName)
-      console.log(event.target.innerHTML)
-      console.log(event.target.type)
+    clickHandler: function($event, message) {
+      this.message = message
+      console.log($event);
     }
   }
 })
