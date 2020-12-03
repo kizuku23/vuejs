39f9420ec9c1f486d87f1cd11559226929809107
vagrant <km.dwc23@gmail.com>
2020-12-03 08:35:41 +0000

    [Finish] section13

--- sample/js/main.js
+++ sample/js/main.js
@@ -1,14 +1,13 @@
-// Vue.component('hello-component', {
-//   template: '<p>Hello</p>'
-// })
-
-var helloComponent = {
-  template: '<p>Hello</p>'
-}
+Vue.component('button-counter', {
+  data: function() {
+    return {
+      count: 0
+    }
+  },
+  // template: '<button v-on:click="count++">{{ count }}</button>'
+  template: '<div><span>count: </span><button v-on:click="count++">{{ count }}</button></div>'
+})
 
 var app = new Vue({
-  el: '#app',
-  components: {
-    'hello-component' :helloComponent
-  }
+  el: '#app'
 })
