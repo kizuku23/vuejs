a7ac039bdae534892524486b378e71b24b094fef
vagrant <km.dwc23@gmail.com>
2020-12-03 08:28:29 +0000

    [Finish] 13-140

--- sample/js/main.js
+++ sample/js/main.js
@@ -1,7 +1,14 @@
+// Vue.component('hello-component', {
+//   template: '<p>Hello</p>'
+// })
+
+var helloComponent = {
+  template: '<p>Hello</p>'
+}
+
 var app = new Vue({
   el: '#app',
-  data: {
-    message: '',
-    age: 0
+  components: {
+    'hello-component' :helloComponent
   }
 })
