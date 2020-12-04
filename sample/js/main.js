97e59b543acff95fd8d4f10eafbfa2408b9f262b
vagrant <km.dwc23@gmail.com>
2020-12-04 06:55:42 +0000

    [Finish] section14

--- sample/js/main.js
+++ sample/js/main.js
@@ -1,13 +1,6 @@
-Vue.component('button-counter', {
-  data: function() {
-    return {
-      count: 0
-    }
-  },
-  // template: '<button v-on:click="count++">{{ count }}</button>'
-  template: '<div><span>count: </span><button v-on:click="count++">{{ count }}</button></div>'
-})
-
 var app = new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    show: false
+  }
 })
